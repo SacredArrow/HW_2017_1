93b89b53ae9e8f0a615fcb478d314ec298862c41
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2017-10-01 15:01:17 +0300

    Updated homework

--- endian.c
+++ endian.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
-union Data{
-  int i;
-  char c[sizeof(int)];
+#include <stdio.h>
+union Data {
+    int  i;
+    char c[sizeof(int)];
 };
 
-int main(int argc, char const *argv[]) {
-  union Data data;
-  data.i=-24;
+int main(int argc, char const * argv[])
+{
+    union Data data;
 
-  printf("%d %d %d %d",data.c[0],data.c[1],data.c[2],data.c[3]);
-  return 0;
+    data.i = 0x01020304;
+
+    printf("%s\n", data.c[0] == 4 ? "little-endian" : "big-endian");
+    return 0;
 }
+
 // #include <stdio.h>
 // unsigned short x = 1; /* 0x0001 */
 // int main(void)
