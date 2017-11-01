4d992b7dcf26b4fc12c63061654073bf372f7830
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2017-11-01 13:40:58 +0300

    Fixed second

--- test/2.c
+++ test/2.c
@@ -2,21 +2,18 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
-  char s1[10000];
-  char s2[10000];
-  scanf("%s\n", s1);
-  scanf("%s", s2);
-  int x = strlen(s1);
-  int y = strlen(s2);
-  if (x != y) {
-    if (x > y) {
-      printf("First > second\n");
-      return 0;
-    } else {
-      printf("Second > first\n");
-      return 0;
-    }
-  }
+  char s1_1[256];
+  char s2_1[256];
+  scanf("%s\n", s1_1);
+  scanf("%s", s2_1);
+  int x = strlen(s1_1);
+  int y = strlen(s2_1);
+  unsigned int s1[256];
+  unsigned int s2[256];
+  for (int i = 0; i < x; i++)
+    s1[i] = (int)s1_1[i];
+  for (int i = 0; i < x; i++)
+    s2[i] = (int)s2_1[i];
   for (int i = 0; i < x; i++) {
     if (s1[i] == s2[i])
       continue;
