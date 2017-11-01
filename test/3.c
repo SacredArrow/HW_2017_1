bc3e02b3eb9a5e75a5663a682de57ce977102994
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2017-11-01 12:15:23 +0300

    Third problem

---
+++ test/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  FILE *file = fopen("example", "r");
+  while (!feof(file)) {
+    char s[256];
+    fgets(s, 256, file);
+    char *x = strstr(s, "//");
+    if (x) {
+      printf("%s", x);
+    }
+  }
+}
